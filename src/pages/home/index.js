e2869b983097dce89f0cfd83d16417d7fa8b6293
motasimfoad <motasimfoad@gmail.com>
2019-11-30 20:41:53 +0600

    Added basic structure of home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from '../../logo.svg';
 import '../home/style.css';
 
 function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Home
-        </a>
+      <div className="App-main">
+        <div className="App-left">
+hi
+        </div>
+        <div className="App-right">
+hi
+        </div>
+      </div>
+      <footer App-footer>
+        Hello
+      </footer>
       </header>
     </div>
   );
