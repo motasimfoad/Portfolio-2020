afefd5a687e26d0d24fadb46d7e74c5e3370d621
motasimfoad <motasimfoad@gmail.com>
2019-11-30 19:20:10 +0600

    Added basic routing functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../src/pages/home';
+import App from '../src/nav';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
