c835b6ce467dedd826b3fb7d5586627fb7fbebbe
motasimfoad <motasimfoad@gmail.com>
2019-12-01 23:54:40 +0600

    Home Right : Added social icons

--- src/components/home_right/index.js
+++ src/components/home_right/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../home_right/style.css';
 import Foad from '../../assets/img/foad.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faGooglePlus, faInstagram,faPinterest, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faTwitter, faInstagram, faGithub, faDribbble } from '@fortawesome/free-brands-svg-icons';
 import {
   Container,
   Row,
@@ -20,7 +20,13 @@ function Home_Right() {
         </Col>
       </Row>
       <Row className="home-right-footer">
-      <Col xl={12}>2 of 3 (wider)</Col>
+      <Col xl={12} className="home-right-footer-icons">
+        <FontAwesomeIcon className="home-right-footer-icons-padding fb" icon={faFacebook} />
+        <FontAwesomeIcon className="home-right-footer-icons-padding git" icon={faGithub} />
+        <FontAwesomeIcon className="home-right-footer-icons-padding twitter" icon={faTwitter} />
+        <FontAwesomeIcon className="home-right-footer-icons-padding insta" icon={faInstagram} />
+        <FontAwesomeIcon className="home-right-footer-icons-padding dribble" icon={faDribbble} />
+      </Col>
       </Row>
     </Container>
   );
