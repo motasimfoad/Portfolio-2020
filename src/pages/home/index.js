30f47f34e4d3e54f89e4d77d361c96316dea6ffa
motasimfoad <motasimfoad@gmail.com>
2019-12-02 00:33:23 +0600

    Home Screen: Added fade in

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,26 +7,27 @@ import {
   Container,
   Row,
   Col,
-} from 'react-bootstrap'
+} from 'react-bootstrap';
+import Animate from 'react-smooth';
 
 function Home() {
   return (
-   
-      <Container className="App-header" fluid={true}>
-        <Row className="App-main">
-        <Col  xl={7}  className="App-left">
-          <HomeLeft />
-        </Col>
-        <Col  xl={5} className="App-right">
-          <HomeRight />
-        </Col>
-        </Row>
-        <Row className="App-footer">
-          <Footer />
-        </Row>
+      <Animate to="1" from="0" attributeName="opacity">
+        <Container className="App-header" fluid={true}>
+          <Row className="App-main">
+          <Col  xl={7}  className="App-left">
+            <HomeLeft />
+          </Col>
+          <Col  xl={5} className="App-right">
+            <HomeRight />
+          </Col>
+          </Row>
+          <Row className="App-footer">
+            <Footer />
+          </Row>
        </Container>
-    
-  );
+      </Animate>
+    );
 }
 
 export default Home;
