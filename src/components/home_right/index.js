f0095545153db22b03a76bd437afaed4de263c59
motasimfoad <motasimfoad@gmail.com>
2019-12-02 00:15:53 +0600

    Home Right : Added social icons styling

--- src/components/home_right/index.js
+++ src/components/home_right/index.js
@@ -21,11 +21,21 @@ function Home_Right() {
       </Row>
       <Row className="home-right-footer">
       <Col xl={12} className="home-right-footer-icons">
+        <a href="https://fb.com/okrittim" target="_blank">
         <FontAwesomeIcon className="home-right-footer-icons-padding fb" icon={faFacebook} />
+        </a>
+        <a href="https://github.com/motasimfoad" target="_blank">
         <FontAwesomeIcon className="home-right-footer-icons-padding git" icon={faGithub} />
+        </a>
+        <a href="http://twitter.com/okrittim" target="_blank">
         <FontAwesomeIcon className="home-right-footer-icons-padding twitter" icon={faTwitter} />
+        </a>
+        <a href="https://www.instagram.com/motasimfoad/" target="_blank">
         <FontAwesomeIcon className="home-right-footer-icons-padding insta" icon={faInstagram} />
+        </a>
+        <a href="https://dribbble.com/charchill" target="_blank">
         <FontAwesomeIcon className="home-right-footer-icons-padding dribble" icon={faDribbble} />
+        </a>
       </Col>
       </Row>
     </Container>
