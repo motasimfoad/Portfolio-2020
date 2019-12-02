c3e97764aab08c8e69f2f56b7d8eaa02bd3cc751
motasimfoad <motasimfoad@gmail.com>
2019-12-02 20:35:30 +0600

    Responsive issues fixed with right home screen part

--- src/components/home_right/index.js
+++ src/components/home_right/index.js
@@ -18,25 +18,25 @@ function Home_Right() {
           <br />
           <Image src={Foad} className="home-right-main-img"/>
         </Col>
-      </Row>
-      <Row className="home-right-footer">
-      <Col xl={12} className="home-right-footer-icons">
-        <a href="https://fb.com/okrittim" target="_blank">
-        <FontAwesomeIcon className="home-right-footer-icons-padding fb" icon={faFacebook} />
-        </a>
-        <a href="https://github.com/motasimfoad" target="_blank">
-        <FontAwesomeIcon className="home-right-footer-icons-padding git" icon={faGithub} />
-        </a>
-        <a href="https://twitter.com/okrittim" target="_blank">
-        <FontAwesomeIcon className="home-right-footer-icons-padding twitter" icon={faTwitter} />
-        </a>
-        <a href="https://www.instagram.com/motasimfoad/" target="_blank">
-        <FontAwesomeIcon className="home-right-footer-icons-padding insta" icon={faInstagram} />
-        </a>
-        <a href="https://dribbble.com/charchill" target="_blank">
-        <FontAwesomeIcon className="home-right-footer-icons-padding dribble" icon={faDribbble} />
-        </a>
-      </Col>
+        <Row className="home-right-footer" >
+          <Col xl={12} className="home-right-footer-icons">
+          <a href="https://fb.com/okrittim" target="_blank">
+          <FontAwesomeIcon className="home-right-footer-icons-padding fb" icon={faFacebook} />
+          </a>
+          <a href="https://github.com/motasimfoad" target="_blank">
+          <FontAwesomeIcon className="home-right-footer-icons-padding git" icon={faGithub} />
+          </a>
+          <a href="https://twitter.com/okrittim" target="_blank">
+          <FontAwesomeIcon className="home-right-footer-icons-padding twitter" icon={faTwitter} />
+          </a>
+          <a href="https://www.instagram.com/motasimfoad/" target="_blank">
+          <FontAwesomeIcon className="home-right-footer-icons-padding insta" icon={faInstagram} />
+          </a>
+          <a href="https://dribbble.com/charchill" target="_blank">
+          <FontAwesomeIcon className="home-right-footer-icons-padding dribble" icon={faDribbble} />
+          </a>
+          </Col>
+        </Row>
       </Row>
     </Container>
   );
