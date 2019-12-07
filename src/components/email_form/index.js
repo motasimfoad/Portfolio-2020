0f4fb6ed870f26b4dc9b797fc2a9ec5d08f98cc1
Motasim Foad <motasimfoad@gmail.com>
2019-12-07 08:40:31 +0600

    Conact Me : Refactored right side

--- src/components/email_form/index.js
+++ src/components/email_form/index.js
@@ -1,21 +1,152 @@
 import React from 'react';
-import '../footer/style.css';
-import {
-  Container,
-  Row,
-  Col,
-} from 'react-bootstrap';
+import '../email_form/style.css';
+import * as emailjs from 'emailjs-com';
+import { Button, Form, FormGroup, Container, Row, Col, Modal, Image } from 'react-bootstrap';
+import Footer from '../../components/footer';
+import Animate from 'react-smooth';
+import Hamburger_Menu from '../../components/hamburger_menu';
+import Contact_Left from '../../components/contact_left'
 
-function Footer() {
-  return (
-    <Container className="footer" fluid={true}>
-      <Row >
-        <Col xl={12}>
-        Copyright © 2018-2019 <strong>Motasim Foad</strong>,  <small>V2.0</small>
-        </Col>
-      </Row>
-    </Container>
-  );
+class Email_Form extends React.Component {
+
+  constructor(props, context) {
+		super(props, context);
+
+		this.handleShow = this.handleShow.bind(this);
+		this.handleClose = this.handleClose.bind(this);
+
+		this.state = {
+			show: false,
+		};
+	}
+
+	handleClose() {
+		this.setState({ show: false });
+	}
+
+	handleShow() {
+		this.setState({ show: true });
+	}
+
+  state = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+    show: false,
+  }
+  
+  handleSubmit(e) {
+    e.preventDefault()    
+    
+    const { name, email, subject, message } = this.state    
+    
+    let templateParams = {
+      from_name: email,
+      to_name: 'motasimfoad@gmail.com',
+      subject: subject,
+      message_html: message,
+     }
+     
+     emailjs.send(
+      'gmail',
+      'template_OErNDyFu',
+       templateParams,
+      'user_64rGh5RuHtigw7paS8NsX'
+     )
+     .then((result) => {
+        this.handleShow();
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });     
+        this.resetForm()
+    }
+    
+    resetForm() {
+    this.setState({
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+      modal: false,
+    })
+  }
+  
+  handleChange = (param, e) => {
+    this.setState({ [param]: e.target.value })
+  }
+  
+  render() {
+    return (
+  
+          <Form onSubmit={this.handleSubmit.bind(this)} className="Contact-form">
+            <p className="contact-form-header">Contact Form</p>
+             <FormGroup controlId="formBasicEmail">
+              <input
+                type="email"
+                name="email"
+                value={this.state.email}
+                className="contact_input"
+                onChange={this.handleChange.bind(this, 'email')}
+                placeholder="Email Address"
+                required
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicName">
+              <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                className="contact_input"
+                onChange={this.handleChange.bind(this, 'name')}
+                placeholder="Your Name"
+                required
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicSubject">
+             <input
+                type="text"
+                name="subject"
+                value={this.state.subject}
+                className="contact_input"
+                onChange={this.handleChange.bind(this, 'subject')}
+                placeholder="Subject"
+                required
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicMessage">
+              <input
+                type="textarea"
+                name="message"
+                value={this.state.message}
+                className="contact_input"
+                onChange={this.handleChange.bind(this, 'message')}
+                placeholder="Your text"
+                required
+              />
+            </FormGroup>
+            <br />
+            <Button variant="outline-light" size="lg" type="submit">
+              Submit
+            </Button>
+            <Modal
+                size="md"
+                aria-labelledby="contained-modal-title-vcenter"
+                show={this.state.show} onHide={this.handleClose}
+                centered
+              >
+            <Modal.Body className="contact_success_modal_body">
+              <Image className="contact_success_modal_img" src="https://icon-library.net/images/success-icon/success-icon-5.jpg" />
+              <h5>Email Successfully Delivered !!</h5>
+              < br />
+              <Button variant="outline-light" size="lg" onClick={this.handleClose}>Close</Button>
+            </Modal.Body>
+           
+          </Modal>
+          </Form>
+    )
+  }
 }
 
-export default Footer;
+export default Email_Form
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,164 +1,26 @@
 import React from 'react';
 import '../contact/style.css';
-import * as emailjs from 'emailjs-com';
-import { Button, Form, FormGroup, Container, Row, Col, Modal, Image } from 'react-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap';
 import Footer from '../../components/footer';
 import Animate from 'react-smooth';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram, faGithub, faDribbble } from '@fortawesome/free-brands-svg-icons';
 import Hamburger_Menu from '../../components/hamburger_menu';
-import Contact_Left from '../../components/contact_left'
+import Contact_Left from '../../components/contact_left';
+import Email_Form from '../../components/email_form';
 
 class Contact extends React.Component {
 
-  constructor(props, context) {
-		super(props, context);
-
-		this.handleShow = this.handleShow.bind(this);
-		this.handleClose = this.handleClose.bind(this);
-
-		this.state = {
-			show: false,
-		};
-	}
-
-	handleClose() {
-		this.setState({ show: false });
-	}
-
-	handleShow() {
-		this.setState({ show: true });
-	}
-
-  state = {
-    name: '',
-    email: '',
-    subject: '',
-    message: '',
-    show: false,
-  }
-  
-  handleSubmit(e) {
-    e.preventDefault()    
-    
-    const { name, email, subject, message } = this.state    
-    
-    let templateParams = {
-      from_name: email,
-      to_name: 'motasimfoad@gmail.com',
-      subject: subject,
-      message_html: message,
-     }
-     
-     emailjs.send(
-      'gmail',
-      'template_OErNDyFu',
-       templateParams,
-      'user_64rGh5RuHtigw7paS8NsX'
-     )
-     .then((result) => {
-        this.handleShow();
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });     
-        this.resetForm()
-    }
-    
-    resetForm() {
-    this.setState({
-      name: '',
-      email: '',
-      subject: '',
-      message: '',
-      modal: false,
-    })
-  }
-  
-  handleChange = (param, e) => {
-    this.setState({ [param]: e.target.value })
-  }
-  
   render() {
     return (
       <Animate to="1" from="0" attributeName="opacity">
         <Container className="Contact-header" fluid={true}>
           <Row className="Contact-main">
-          <Hamburger_Menu />
+            <Hamburger_Menu />
           <Col  xl={6}  className="Contact-left">
            <Contact_Left />
           </Col>
-
           <Col  xl={6}  className="Contact-right">
-            
-          <Form onSubmit={this.handleSubmit.bind(this)} className="Contact-form">
-          <p className="contact-form-header">Contact Form</p>
-             <FormGroup controlId="formBasicEmail">
-              
-               <input
-                type="email"
-                name="email"
-                value={this.state.email}
-                className="contact_input"
-                onChange={this.handleChange.bind(this, 'email')}
-                placeholder="Email Address"
-                required
-              />
-            </FormGroup><FormGroup controlId="formBasicName">
-              
-              <input
-                type="text"
-                name="name"
-                value={this.state.name}
-                className="contact_input"
-                onChange={this.handleChange.bind(this, 'name')}
-                placeholder="Your Name"
-                required
-              />
-            </FormGroup><FormGroup controlId="formBasicSubject">
-             
-              <input
-                type="text"
-                name="subject"
-                value={this.state.subject}
-                className="contact_input"
-                onChange={this.handleChange.bind(this, 'subject')}
-                placeholder="Subject"
-                required
-              />
-            </FormGroup><FormGroup controlId="formBasicMessage">
-              
-              <input
-                type="textarea"
-                name="message"
-                value={this.state.message}
-                className="contact_input"
-                onChange={this.handleChange.bind(this, 'message')}
-                placeholder="Your text"
-                required
-              />
-            </FormGroup>
-            <br />
-            
-            <Button variant="outline-light" size="lg" type="submit">
-              Submit
-            </Button>
-          </Form>
-          <Modal
-            size="md"
-            aria-labelledby="contained-modal-title-vcenter"
-            show={this.state.show} onHide={this.handleClose}
-            centered
-          >
-            <Modal.Body className="contact_success_modal_body">
-              <Image className="contact_success_modal_img" src="https://icon-library.net/images/success-icon/success-icon-5.jpg" />
-              <h5>Email Successfully Delivered !!</h5>
-              < br />
-              <Button variant="outline-light" size="lg" onClick={this.handleClose}>Close</Button>
-            </Modal.Body>
-           
-          </Modal>
-            </Col>
+            <Email_Form />
+          </Col>
           </Row>
           <Row className="Contact-footer">
             <Footer />
