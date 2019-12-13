f1917f8e2aebf4ccbb5403e21181e6b1b02adb0a
Motasim Foad <motasimfoad@gmail.com>
2019-12-14 02:35:31 +0600

    Added photo viewer

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -4,6 +4,32 @@ import HamburgerMenu from '../../components/hamburger_menu';
 import {Container, Row, Col} from 'react-bootstrap';
 import Footer from '../../components/footer';
 import Animate from 'react-smooth';
+import Gallery from 'react-grid-gallery';
+import cv1 from '../../assets/img/cv1.png'
+import cv2 from '../../assets/img/cv2.png'
+import cv3 from '../../assets/img/cv3.png'
+import cv4 from '../../assets/img/cv4.png'
+
+const IMAGES =
+[
+  {
+    src: cv1,
+    thumbnail: cv1,
+    
+    tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+    caption: "Boats (Jeshu John - designerspics.com)"
+},
+{
+  src: cv2,
+ },
+{
+  src: cv3,
+},
+{
+  src: cv4,
+ }
+]
+
 
 class About extends Component {
  
@@ -20,7 +46,7 @@ class About extends Component {
           </Col>
           <Col  xl={6}  className="About-right">
            
-            hi
+          <Gallery className="About-gallery" images={IMAGES}/>,
          
           </Col>
           </Row>
