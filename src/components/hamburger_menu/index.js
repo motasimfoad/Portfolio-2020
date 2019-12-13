b66ecda4a031f44826349787aa04b91b60491700
Motasim Foad <motasimfoad@gmail.com>
2019-12-13 07:36:02 +0600

    Prod build modification 13th december

--- src/components/hamburger_menu/index.js
+++ src/components/hamburger_menu/index.js
@@ -14,7 +14,7 @@ function Hamburger() {
               <div>
                 <ul>
                   <li><Link to="/home">Home</Link></li>
-                  <li><Link to="/about">About</Link></li>
+                  {/* <li><Link to="/about">About</Link></li> */}
                   <li><Link to="/contact">Contact</Link></li>
                 </ul>
                </div>
