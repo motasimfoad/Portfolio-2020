c03628ec619c017ecd32e99b222b9dbd26640698
Motasim Foad <motasimfoad@gmail.com>
2019-12-18 05:15:08 +0600

    SEO : 5

--- src/components/contact_left/index.js
+++ src/components/contact_left/index.js
@@ -8,7 +8,7 @@ function Contact_Left() {
     <div>
            <Row className="Contact-text">
               <Col xl={12}>
-              <p className="p-heading1">Get'in Touch</p>
+              <h1 className="p-heading1">Get'in Touch</h1>
               <p className="p-heading2">
               Holla <strong>Visitor !!</strong><br />
               Thank you for bumping in. If you have any queries/idea/project, feel free to <strong>PING</strong> me.
