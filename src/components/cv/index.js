d2132acaabf20bff557f2a75162b1be3f9d71476
Motasim Foad <motasimfoad@gmail.com>
2019-12-19 04:47:03 +0600

    About CV : left main content refactor 1

--- src/components/cv/index.js
+++ src/components/cv/index.js
@@ -6,6 +6,13 @@ import {
   Col,
 } from 'react-bootstrap';
 import HamburgerMenu from '../hamburger_menu';
+import cv1 from '../../assets/img/cv1.png';
+import CLC from '../cv_left_content';
+
+
+function lala(){
+  alert("lalal")
+}
 
 function CV() {
   return (
@@ -15,18 +22,10 @@ function CV() {
       </Row>
       <Row className="cv_content">
         <Col xl={6} className="cv_left">
-        <h1><u>About</u></h1>
-        <h3>
-        Accidental Product Manager with Software Engineering base
-        </h3>
-        <p>
-        Experienced Project & Product Manager with a demonstrated history of working in computer software industry. 
-        Skilled in leading edge project management, web & software development tools & trends. 
-        Strong computer engineering base with a BSc in Computer Science focused in Computer Software Engineering from BRAC University.
-        </p>
+          <CLC />
         </Col>
         <Col xl={6} className="cv_right">
-        CV
+        <img src={cv1} onClick={lala} style={{height:"80%", width:"70%"}} />
         </Col>
       </Row>
     </Container>
---
+++ src/components/cv_left_content/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../cv_left_content/style.css';
+import {
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap';
+
+function CLC() {
+  return (
+   <div>
+        <h1><u>About</u></h1>
+        <h3>
+        Accidental Product Manager with Software Engineering base
+        </h3>
+        <p>
+        Experienced Project & Product Manager with a demonstrated history of working in computer software industry. 
+        Skilled in leading edge project management, web & software development tools & trends. 
+        Strong computer engineering base with a BSc in Computer Science focused in Computer Software Engineering from BRAC University.
+        </p>
+   </div>
+  );
+}
+
+export default CLC;
