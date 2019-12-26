239b3c74273ee1dc6aed8e74dcf20d1e42c4884e
Motasim Foad <motasimfoad@gmail.com>
2019-12-27 04:32:21 +0600

    Added and splitted data.json

--- src/components/works/index.js
+++ src/components/works/index.js
@@ -8,7 +8,7 @@ import {
   Button
 } from 'react-bootstrap';
 import HamburgerMenu from '../hamburger_menu';
-import Data from '../../data/data.json'
+import Data from '../../data/employer_data.json'
 
 function Works() {
   return (
@@ -16,30 +16,33 @@ function Works() {
     <Row>
        <HamburgerMenu />
     </Row>
+    <Row>
+       <div>
+         hi
+       </div>
+    </Row>
     <Row className="works_content">
       
     {
        Data.employer.map(function(item, i){
          return(
-   <Col xl={4} key={i+1}>
-   <Card style={{ width: '18rem' }}>
-   
-         <Card.Img variant="top" src="holder.js/100px180" />
-         <Card.Body>
-<Card.Title>{item.name}</Card.Title>
-           <Card.Text>
-           {item.docs}
-           {item.logo}
-           </Card.Text>
-           <Button variant="primary">Go somewhere</Button>
-         </Card.Body>
-       </Card>
-  </Col>
+
+        <Col xl={4} key={i+1}>
+              <Card style={{ width: '18rem' }}>
+              
+              <Card.Img variant="top" src={item.logo} />
+              <Card.Body>
+              <Card.Title>{item.name}</Card.Title>
+                <Card.Text>
+                {item.docs}
+                {item.logo}
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+        </Col>
          )
-        
-        
-     
-      })
+       })
      }
 
     
