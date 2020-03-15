ded03671ad13e74becf4bfc46fd8d5a2cd0fa5f8
Motasim Foad <motasimfoad@gmail.com>
2020-03-15 20:46:03 +0600

    Projects : git api added

--- src/components/project_card_details/index.js
+++ src/components/project_card_details/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import '../project_card_details/style.css';
 import {
   Col,
@@ -9,38 +9,80 @@ import {
   ButtonGroup
 } from 'react-bootstrap';
 
-function PCD() {
-  return (
-  
-      
-      <Card style={{ width: '22rem' }}>
-      
-      <Card.Body>
-      <Row>
-      <Col xs={4} className="project_card_left">
-      <Image className="project_card_left_img" src="https://image.flaticon.com/icons/svg/25/25231.svg" roundedCircle />
-      </Col>
-      <Col xs={8} className="project_card_right">
-        <Card.Text>
-          Name : Repo name < br />
-          Git Location : Location < br />
-          Created at : Time date < br />
-          Last Updated : Time date < br />
-          
-          <ButtonGroup aria-label="Basic example">
-            <Button variant="dark">SSH</Button>
-            <Button variant="dark">HTTPS</Button>
-            <Button variant="dark">View</Button>
-          </ButtonGroup>
-        </Card.Text>
-      </Col>
-      </Row>
-      </Card.Body>
-     
-      </Card> 
-     
-    
-  );
-}
+const PCD = () => {
+  const [hasError, setErrors] = useState(false);
+  const [planets, setPlanets] = useState([]);
 
+  async function fetchData() {
+    const res = await fetch("https://api.github.com/users/motasimfoad/repos");
+    res
+      .json()
+      .then(res => setPlanets(res))
+      .catch(err => setErrors(err));
+  }
+
+  useEffect(() => {
+    fetchData();
+  });
+  
+  
+
+  return (
+    <div>
+      <ul>
+        {planets.map(item => (
+          <li key = {item.id}>{item.name}</li>
+        ))
+
+        }
+      </ul>
+       {/* {
+        planets.map(function(item, i){
+          return(
+            <Col xl={4} className="works_content_col" key={i+1}>
+              <Card style={{ width: '20rem' }} >
+                <Card.Img variant="top" src={item.name} />
+                <Card.Body>
+                <Card.Title>{item.id}</Card.Title>
+                  <Card.Text>
+                  {item.name}
+                  {item.name}
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+              </Col>
+          )
+        })
+        } */}
+    </div>
+
+      // <Card style={{ width: '22rem' }}>
+            
+      // <Card.Body>
+      // <Row>
+      // <Col xs={4} className="project_card_left">
+      // <Image className="project_card_left_img" src="https://image.flaticon.com/icons/svg/25/25231.svg" roundedCircle />
+      // </Col>
+      // <Col xs={8} className="project_card_right">
+      //   <Card.Text>
+      //     Name : Repo name < br />
+      //     Git Location : Location < br />
+      //     Created at : Time date < br />
+      //     Last Updated : Time date < br />
+          
+          
+      //       <Button variant="dark">SSH</Button>
+      //       <Button variant="dark">HTTPS</Button>
+      //       <Button variant="dark">View</Button>
+          
+      //   </Card.Text>
+      // </Col>
+      // </Row>
+      // </Card.Body>
+
+      // </Card> 
+
+  );
+};
 export default PCD;
