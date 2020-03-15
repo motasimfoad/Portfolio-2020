0640491f3b4cd03fb042dfba3ba11f784cc00b62
Motasim Foad <motasimfoad@gmail.com>
2020-03-15 21:06:28 +0600

    Projects : git api expanded

--- src/components/project_card_details/index.js
+++ src/components/project_card_details/index.js
@@ -29,59 +29,45 @@ const PCD = () => {
 
   return (
     <div>
-      <ul>
+     
         {planets.map(item => (
-          <li key = {item.id}>{item.name}</li>
+         
+          <Card style={{ width: '22rem' }} key = {item.id}>
+            
+            <Card.Body>
+            <Row>
+            <Col xs={4} className="project_card_left">
+            <Image className="project_card_left_img" src="https://image.flaticon.com/icons/svg/25/25231.svg" roundedCircle />
+            </Col>
+            <Col xs={8} className="project_card_right">
+              <Card.Text>
+                Name : {item.name} < br />
+                Git Location : {item.full_name} < br />
+                Created at : {item.created_at} < br />
+                Last Updated : {item.updated_at} < br />
+                
+                
+                  <Button variant="dark">SSH</Button>
+                  <Button variant="dark">HTTPS</Button>
+                  <Button variant="dark"> <a href={item.svn_url} target="_blank">View</a></Button>
+                
+              </Card.Text>
+            </Col>
+            </Row>
+            </Card.Body>
+      
+            </Card>
         ))
 
         }
-      </ul>
-       {/* {
-        planets.map(function(item, i){
-          return(
-            <Col xl={4} className="works_content_col" key={i+1}>
-              <Card style={{ width: '20rem' }} >
-                <Card.Img variant="top" src={item.name} />
-                <Card.Body>
-                <Card.Title>{item.id}</Card.Title>
-                  <Card.Text>
-                  {item.name}
-                  {item.name}
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-              </Col>
-          )
-        })
-        } */}
+     
+
+
+ 
+      
     </div>
 
-      // <Card style={{ width: '22rem' }}>
-            
-      // <Card.Body>
-      // <Row>
-      // <Col xs={4} className="project_card_left">
-      // <Image className="project_card_left_img" src="https://image.flaticon.com/icons/svg/25/25231.svg" roundedCircle />
-      // </Col>
-      // <Col xs={8} className="project_card_right">
-      //   <Card.Text>
-      //     Name : Repo name < br />
-      //     Git Location : Location < br />
-      //     Created at : Time date < br />
-      //     Last Updated : Time date < br />
-          
-          
-      //       <Button variant="dark">SSH</Button>
-      //       <Button variant="dark">HTTPS</Button>
-      //       <Button variant="dark">View</Button>
-          
-      //   </Card.Text>
-      // </Col>
-      // </Row>
-      // </Card.Body>
-
-      // </Card> 
+     
 
   );
 };
