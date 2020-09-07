9e4c63c185acbe044ca73e9481c05498863c8e66
Motasim Foad <motasimfoad@gmail.com>
2020-09-07 20:22:43 +0600

    project page : added style for data source and code refactor

--- src/components/project_main/index.js
+++ src/components/project_main/index.js
@@ -18,196 +18,26 @@ function PM() {
   return (
       <Row xl={12} className="main_row">
 
-{info.data.map(item => ( 
-
-<Col xl={4}  key={item.name} className="card_col">
-<Card className="card_main">
-  <blockquote className="blockquote mb-0 card-body">
-    <h2>
-    {item.name}
-    </h2>
-    <footer className="blockquote-footer">
-        built using <strong title="Source Title">ReactJS, HASURA</strong>
-    </footer>
-  </blockquote>
-  <Card.Body>
-  <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faImages} /></Card.Link>
-  <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faLink} /></Card.Link>
-</Card.Body>
-</Card>
-</Col>
-
-
-))}
-        
-  {/* <Col xl={4} style={{padding:'5%'}} >
-  <Card className="card_main">
-    <blockquote className="blockquote mb-0 card-body">
-      <h2>
-        Internet addiction test
-      </h2>
-      <footer className="blockquote-footer">
-          built using <strong title="Source Title">ReactJS, HASURA</strong>
-      </footer>
-    </blockquote>
-    <Card.Body>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faImages} /></Card.Link>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faLink} /></Card.Link>
-  </Card.Body>
-  </Card>
-  </Col> */}
-
-
-  {/* <Col xl={4} style={{padding:'5%'}} >
-  <Card className="card_main">
-    <blockquote className="blockquote mb-0 card-body">
-      <h2>
-        Internet addiction test
-      </h2>
-      <footer className="blockquote-footer">
-          built using <strong title="Source Title">ReactJS, HASURA</strong>
-      </footer>
-    </blockquote>
-    <Card.Body>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faImages} /></Card.Link>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faLink} /></Card.Link>
-  </Card.Body>
-  </Card>
-  </Col>
-  <Col xl={4} style={{padding:'5%'}} >
-  <Card className="card_main">
-    <blockquote className="blockquote mb-0 card-body">
-      <h2>
-        Internet addiction test
-      </h2>
-      <footer className="blockquote-footer">
-          built using <strong title="Source Title">ReactJS, HASURA</strong>
-      </footer>
-    </blockquote>
-    <Card.Body>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faImages} /></Card.Link>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faLink} /></Card.Link>
-  </Card.Body>
-  </Card>
-  </Col>
-  <Col xl={4} style={{padding:'5%'}} >
-  <Card className="card_main">
-    <blockquote className="blockquote mb-0 card-body">
-      <h2>
-        Internet addiction test
-      </h2>
-      <footer className="blockquote-footer">
-          built using <strong title="Source Title">ReactJS, HASURA</strong>
-      </footer>
-    </blockquote>
-    <Card.Body>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faImages} /></Card.Link>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faLink} /></Card.Link>
-  </Card.Body>
-  </Card>
-  </Col>
-  <Col xl={4} style={{padding:'5%'}} >
-  <Card className="card_main">
-    <blockquote className="blockquote mb-0 card-body">
-      <h2>
-        Internet addiction test
-      </h2>
-      <footer className="blockquote-footer">
-          built using <strong title="Source Title">ReactJS, HASURA</strong>
-      </footer>
-    </blockquote>
-    <Card.Body>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faImages} /></Card.Link>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faLink} /></Card.Link>
-  </Card.Body>
-  </Card>
-  </Col>
-  <Col xl={4} style={{padding:'5%'}} >
-  <Card className="card_main">
-    <blockquote className="blockquote mb-0 card-body">
-      <h2>
-        Internet addiction test
-      </h2>
-      <footer className="blockquote-footer">
-          built using <strong title="Source Title">ReactJS, HASURA</strong>
-      </footer>
-    </blockquote>
-    <Card.Body>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faImages} /></Card.Link>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faLink} /></Card.Link>
-  </Card.Body>
-  </Card>
-  </Col>
-  <Col xl={4} style={{padding:'5%'}} >
-  <Card className="card_main">
-    <blockquote className="blockquote mb-0 card-body">
-      <h2>
-        Internet addiction test
-      </h2>
-      <footer className="blockquote-footer">
-          built using <strong title="Source Title">ReactJS, HASURA</strong>
-      </footer>
-    </blockquote>
-    <Card.Body>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faImages} /></Card.Link>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faLink} /></Card.Link>
-  </Card.Body>
-  </Card>
-  </Col>
-  <Col xl={4} style={{padding:'5%'}} >
-  <Card className="card_main">
-    <blockquote className="blockquote mb-0 card-body">
-      <h2>
-        Internet addiction test
-      </h2>
-      <footer className="blockquote-footer">
-          built using <strong title="Source Title">ReactJS, HASURA</strong>
-      </footer>
-    </blockquote>
-    <Card.Body>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faImages} /></Card.Link>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faLink} /></Card.Link>
-  </Card.Body>
-  </Card>
-  </Col>
-  <Col xl={4} style={{padding:'5%'}} >
-  <Card className="card_main">
-    <blockquote className="blockquote mb-0 card-body">
-      <h2>
-        Internet addiction test
-      </h2>
-      <footer className="blockquote-footer">
-          built using <strong title="Source Title">ReactJS, HASURA</strong>
-      </footer>
-    </blockquote>
-    <Card.Body>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faImages} /></Card.Link>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faLink} /></Card.Link>
-  </Card.Body>
-  </Card>
-  </Col>
-  <Col xl={4} style={{padding:'5%'}} >
-  <Card className="card_main">
-    <blockquote className="blockquote mb-0 card-body">
-      <h2>
-        Internet addiction test
-      </h2>
-      <footer className="blockquote-footer">
-          built using <strong title="Source Title">ReactJS, HASURA</strong>
-      </footer>
-    </blockquote>
-    <Card.Body>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faImages} /></Card.Link>
-    <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faLink} /></Card.Link>
-  </Card.Body>
-  </Card>
-  </Col> */}
-
-  
-  
- 
-
-      </Row>
+      {info.data.map(item => ( 
+      <Col xl={4}  key={item.name} className="card_col">
+      <Card className="card_main">
+        <blockquote className="blockquote mb-0 card-body">
+          <h2>
+          {item.name}
+          </h2>
+          <footer className="blockquote-footer">
+              built using <strong title="Source Title">ReactJS, HASURA</strong>
+          </footer>
+        </blockquote>
+        <Card.Body>
+        <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faImages} /></Card.Link>
+        <Card.Link href="#"><FontAwesomeIcon className="icon" size="lg" icon={faLink} /></Card.Link>
+      </Card.Body>
+      </Card>
+      </Col>
+      ))}
+      
+    </Row>
   );
 }
 
