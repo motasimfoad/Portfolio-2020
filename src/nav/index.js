1c1562a2c7ec1d452fb365c1fd275acbcf20b8ad
LCB <ckdqja5815@naver.com>
2023-06-19 03:17:19 +0900

    react hook 업데이트

--- src/nav/index.js
+++ src/nav/index.js
@@ -1,10 +1,6 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "../pages/home";
 import About from "../pages/about";
 import Contact from "../pages/contact";
@@ -14,12 +10,12 @@ import Project from "../pages/projects";
 export default function Nav() {
   return (
     <Router>
-     <Switch>
+      <Switch>
         <Route exact path="/" component={Loading} />
         <Route path="/home" component={Home} />
         <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/project" component={Project} />
+        <Route path="/INSTAGRAM" component={Contact} />
+        <Route path="/My Hobby" component={Project} />
         <Route path="*" component={Home} />
       </Switch>
     </Router>
--- src/pages/loading/index.js
+++ src/pages/loading/index.js
@@ -1,48 +1,40 @@
-import React, { Component } from 'react';
-import '../loading/style.css';
-import { Redirect } from 'react-router-dom';
-import ReactGA from 'react-ga';
-import {Helmet} from 'react-helmet';
+import React, { useEffect, useState } from "react";
+import "../loading/style.css";
+import { Redirect } from "react-router-dom";
+import ReactGA from "react-ga";
+import { Helmet } from "react-helmet";
 import PuffLoader from "react-spinners/PuffLoader";
 
-class Loading extends Component {
+function Loading() {
+  const [redirect, setRedirect] = useState(false);
 
-  state = {
-    redirect: false
-  }
+  useEffect(() => {
+    const id = setTimeout(() => setRedirect(true), 3000);
 
-  componentDidMount() {
-     this.id = setTimeout(() => this.setState({ redirect: true }), 3000);
-    //--Motasim Foads Portfolio website Google analytics--
-    //--Replace with your own key !!
     ReactGA.initialize("UA-154721739-1");
-    ReactGA.pageview('motasimfoad.com - Loading Screen');
-    //--Motasim Foads Portfolio website Google analytics--
-  }
-    
-  componentWillUnmount() {
-    clearTimeout(this.id)
-  }
+    ReactGA.pageview("motasimfoad.com - Loading Screen");
 
-  render() {
-    return this.state.redirect
-      ? <Redirect to="/home" />
-      : <div>
-         <Helmet>
-                <meta charSet="utf-8" />
-                <title>CHANG BEOM IM</title>
-                <link rel="canonical" href="http://motasimfoad.com/" />
-                <meta name="description" content="Motasim Foad - Product Manager | Project Manager | Software Engineer" />
-         </Helmet>
-        <div className="Loading-header">
-          <PuffLoader
-            size={'40vw'}
-            color={"#fff"}
-            loading={true}
-          />
-        </div>
-        </div>
-  }
+    return () => clearTimeout(id);
+  }, []);
+
+  return redirect ? (
+    <Redirect to="/home" />
+  ) : (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>CHANG BEOM IM</title>
+        <link rel="canonical" href="http://motasimfoad.com/" />
+        <meta
+          name="description"
+          content="Motasim Foad - Product Manager | Project Manager | Software Engineer"
+        />
+      </Helmet>
+      <div className="Loading-header">
+        <PuffLoader size={"40vw"} color={"#fff"} loading={redirect} />
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
